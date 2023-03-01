b07fb6f3ec99400ffde72f6b06a15d7eb86a23a9
xavier.oduor <xavier.oduor@turnkeyafrica.com>
2023-03-01 15:18:11 +0300

    angular routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FirstComponent} from "./first/first.component";
+import {SecondComponent} from "./second/second.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: "/first", pathMatch: "full"},
+  {path: "first", component:FirstComponent},
+  {path: "second", component: SecondComponent},
+  {
+    path:"**",
+    component:PageNotFoundComponent,
+    children: [{path:"''", redirectTo: "/first", pathMatch: "full"}]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FirstComponent, SecondComponent];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routing-demo';
+
+  constructor(private router:Router) {
+  }
+  goToFirstComponent() {
+    this.router.navigate(["/first"])
+
+  }
+
+  goToSecondComponent() {
+    this.router.navigate(["/second"])
+  }
 }
